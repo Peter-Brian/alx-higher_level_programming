be33366f736607b76e8b0e200660a82b9cfa46c7
Peter-Brian <studygroup226@gmail.com>
2022-10-12 14:25:00 +0000

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""This module contain the rectangle class."""
+"""Empty Class Square"""
 
 
-class Rectangle:
-    """Empty class Rectangle that defines a rectangle."""
+class Square:
+    """Empty Class Square"""
     pass
