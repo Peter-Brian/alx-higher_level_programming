24fb09391d4d51e00c541a3a932a42281ddf470c
Peter-Brian <studygroup226@gmail.com>
2022-10-19 17:55:13 +0000

    #pythonic

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(prmList=[]):
-    prmList += ["Holberton"]
-    return (", ".join(prmList))
+def magic_string(H=[]):
+    H += ["Holberton"]
+    return (", ".join(H))
