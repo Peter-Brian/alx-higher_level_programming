1c6da7dda9a3b9f486ab8140a9486851b4865481
Peter-Brian <studygroup226@gmail.com>
2022-10-26 08:49:16 +0000

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_lines function
-"""
+"""Module for number_of_lines function."""
 
 
 def number_of_lines(filename=""):
-    """returns the number of lines of a text file"""
-    with open(filename, 'r', encoding='utf8') as f:
-        i = 0
-        for line in f:
-            i += 1
-        return i
-
+    """This function return the number of lines of a text file."""
+    with open(filename) as f:
+        total = 0
+        for run in enumerate(f):
+            total = total + 1
+    return total
