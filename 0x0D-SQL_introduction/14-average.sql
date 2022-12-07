75898b49e696f7141e7360dcab82291d5795b77f
Peter-Brian <studygroup226@gmail.com>
2022-12-07 06:47:09 -0800

    git

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,5 @@
---  The result column name should be average
-The database name will be passed as an argument of the mysql command
-SELECT AVG(score) AS average FROM second_table
+-- script that computes the score average of all records in the table
+-- second_table
+SELECT AVG(score)
+AS average
+FROM second_table;
