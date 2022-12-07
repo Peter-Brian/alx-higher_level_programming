5147c7355b62d0c44804381d22e3498fd0c7b135
PeterBrian <106863315+Peter-Brian@users.noreply.github.com>
2022-12-07 18:37:27 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,5 @@
--- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- creates the MySQL server user user_0d_1
+-- creates a new user
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+-- grants privileges
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
