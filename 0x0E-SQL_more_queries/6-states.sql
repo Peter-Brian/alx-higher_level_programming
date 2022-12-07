9920522568e94784ec5fc799e308b9980f72cc1a
Peter-Brian <studygroup226@gmail.com>
2022-12-07 07:45:50 -0800

    Stable table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- script that creates the table unique_id on your MySQL server
+-- unique id
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	    id INT UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	    name VARCHAR(256) NOT NULL);
