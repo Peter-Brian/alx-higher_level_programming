0b12a43b68d1f42850589bd0369182004cbcd1dd
Peter-Brian <studygroup226@gmail.com>
2022-12-07 07:36:08 -0800

    Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,2 @@
+-- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
