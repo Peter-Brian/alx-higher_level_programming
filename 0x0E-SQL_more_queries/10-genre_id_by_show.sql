10108ee96a9dfe6660201d7c96778ece240f1fc3
Peter-Brian <studygroup226@gmail.com>
2022-12-07 07:53:04 -0800

    Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL serve
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
