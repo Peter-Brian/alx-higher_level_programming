430dbde5c17c0be04c44af6b7649bccf17b6868e
PeterBrian <106863315+Peter-Brian@users.noreply.github.com>
2023-01-16 20:38:43 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-const s = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < 3; i++) {
-  console.log(s[i]);
-}
+//  script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+do {
+  console.log(languages[i]);
+  i++;
+} while (i <= 2);
