d0baad308f68496f958d7c103e1b74cbdeab1041
PeterBrian <106863315+Peter-Brian@users.noreply.github.com>
2023-01-16 20:33:30 +0300

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const argv = process.argv;
+/**
+ * prints two arguments passed to it, in the following format: “ is ”
+ */
 
-const nArgv = argv[2] + ' is ' + argv[3];
-
-console.log(nArgv);
+console.log(process.argv[2] + ' is ' + process.argv[3]);
