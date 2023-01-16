534f62909f21a956237376e0d9fb58b577c33b2a
PeterBrian <106863315+Peter-Brian@users.noreply.github.com>
2023-01-16 20:34:54 +0300

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,17 @@
 #!/usr/bin/node
+// script that prints two arguments passed to it, in the following format: “ is ”
+const { argv } = require('process');
+let first;
+let second;
 
-/**
- * prints two arguments passed to it, in the following format: “ is ”
- */
-
-console.log(process.argv[2] + ' is ' + process.argv[3]);
+if (argv[2] === undefined) {
+  first = 'undefined';
+} else {
+  first = argv[2];
+}
+if (argv[3] === undefined) {
+  second = 'undefined';
+} else {
+  second = argv[3];
+}
+console.log(`${first} is ${second}`);
