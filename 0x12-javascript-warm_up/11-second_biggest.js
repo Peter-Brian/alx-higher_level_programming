ef2144c22238852268e60e6b775b76c60b008059
PeterBrian <106863315+Peter-Brian@users.noreply.github.com>
2023-01-16 20:46:44 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-// script that searches the second biggest integer in the list of arguments
-
-const { argv } = require('process');
-const argc = argv.length - 2;
-let second = 0;
-if (argc >= 2) {
-  const numbers = [];
-  for (let i = 0; i < argc; i++) {
-    numbers.push(argv[i + 2]);
-  }
-  numbers.sort((a, b) => a - b);
-  second = numbers[argc - 2];
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
-console.log(`${second}`);
